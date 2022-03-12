0cb51f8d7f261f82e57a0a9965695dc0062d0f60
Chala Bekabil <chalabekabil@gmail.com>
2022-03-12 16:33:35 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# ros_project
+# A Multi-Strategy Wheeled Robot System for Navigation
+
+Prepared by
+ 1. Chala Bekabil
+ 2. Robbel Habtamu
+
+## Launch the robot
+
+After sourcing
+```
+roslaunch mybot gazebo.launch
+```
+
+## Commands used 
+
+To run motor controller node, open new terminal
+```
+source devel/setup.bash
+rosrun mybot motor_controller
+```
+
+To run controller node (To move the robot)
+```
+rosrun mybot controller.py
+```
+
+To see the ROS node tree
+```
+rqt_graph
+```
---
+++ controller.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+
+import rospy
+import numpy as np
+from nav_msgs.msg import Odometry
+from std_msgs.msg import String
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import Point
+from math import atan2
+
+class Node():
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def astar(maze, start, end):
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
+
+maze = [[1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1]]
+
+in_x = 2
+in_y = 6
+
+x = float(in_x / 2)
+y = float(in_y / 2)
+
+
+theta = 0.0
+command = "STOP"
+def newOdom(msg):
+    global x
+    global y
+    global theta
+
+    x = msg.pose.pose.position.x
+    y = msg.pose.pose.position.y
+
+    rot_q = msg.pose.pose.orientation
+    (roll, pitch, theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
+
+rospy.init_node("speed_controller")
+
+sub = rospy.Subscriber("/odom", Odometry, newOdom)
+pub = rospy.Publisher("/motor_commands", String, queue_size = 1)
+
+
+r = rospy.Rate(4)
+
+goal = Point()
+
+goal.x = 6.5
+goal.y = 3.0
+
+g_x = int(goal.x * 2)
+g_y = int(goal.y * 2)
+
+
+start = (in_x, in_y)
+end = (g_x, g_y)
+path = astar(maze, start, end)
+
+print(path)
+
+path = list(path)
+an_array = np.array(path)
+path = an_array * 0.5
+path = path[::-1]
+path = list(path)
+removed = path.pop()
+
+while path:
+    if not path:
+        command = "STOP"
+    else:
+        removed = path.pop()
+    local_x = removed[0]
+    local_y = removed[1]
+    while not rospy.is_shutdown():
+        
+        inc_x = local_x - x
+        inc_y = local_y - y
+
+        angle_to_goal = atan2(inc_y, inc_x)
+
+        angle = angle_to_goal - theta
+        if angle < -0.1 and command != "STOP":
+            command = "RIGHT"
+        elif angle > 0.1 and command != "STOP":
+            command = "LEFT"
+        else:
+            command = "GO"
+        print(local_x, local_y)
+        if x >= (local_x - 0.1) and x <= (local_x + 0.1) and y >= (local_y - 0.1) and y <= (local_y + 0.1):
+            if not path:
+                command = "STOP"
+                pub.publish(command)
+            break
+
+        pub.publish(command)
+        r.sleep()
